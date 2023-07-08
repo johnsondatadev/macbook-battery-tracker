65924343e878eca1915c6a1725f8374fb8008536
johnsondatadev <ob.johnson.datadev@gmail.com>
2023-07-08 19:53:19 -0400

    Add daily log file

--- schedules/execute_script.py
+++ schedules/execute_script.py
@@ -4,20 +4,19 @@ import constants
 
 
 def execute_script():
+    schedule_script_execution()
     subprocess.call(["python", constants.APP_PATH])
 
 
 def schedule_script_execution():
     while True:
         current_time = time.strftime("%H:%M:%S")
-        if current_time in ["02:00:00", "09:00:00", "12:00:00", "15:00:00", "18:00:00", "21:00:00", "00:00:00"]:
+        if current_time in ["02:00:00", "09:00:00", "12:00:00", "15:00:00", "19:36:00", "21:00:00", "00:00:00"]:
             execute_script()
         time.sleep(1)
 
 
 def run():
-    # print("Script executing...")
-    # schedule_script_execution()
     execute_script()
 
 
